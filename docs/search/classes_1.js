32d0e5da47b9cddf9b2b76605a3e3faf54f38078
EgorNuzhnykh <nuzhnykhev@gmail.com>
2018-11-07 13:11:58 +0300

    Added docs for documentation

---
+++ docs/search/classes_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['elasticsearchlogger',['ElasticSearchLogger',['../class_pip_services_1_1_elastic_search_1_1_log_1_1_elastic_search_logger.html',1,'PipServices3::ElasticSearch::Log']]]
+];
---
+++ docs/search/functions_0.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['closeasync',['CloseAsync',['../class_pip_services_1_1_elastic_search_1_1_log_1_1_elastic_search_logger.html#a0962a05041f8439572e4cf4713badd6e',1,'PipServices3::ElasticSearch::Log::ElasticSearchLogger']]],
+  ['configure',['Configure',['../class_pip_services_1_1_elastic_search_1_1_log_1_1_elastic_search_logger.html#a55110274f8b07eb533ebdf472121f9f0',1,'PipServices3::ElasticSearch::Log::ElasticSearchLogger']]]
+];
