475894c81c2444c52d209e94b65b73a53c291564
banalna <judas.priest999@gmail.com>
2020-12-03 13:36:22 +0300

    delete component.conf.ps1 and doc folder

--- doc/api/search/all_7.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['write',['Write',['../class_pip_services_1_1_elastic_search_1_1_log_1_1_elastic_search_logger.html#a3bc9a0426e3d930a50754ed9a31c735e',1,'PipServices3::ElasticSearch::Log::ElasticSearchLogger']]]
-];
